0487ed450995b2d8d1e61d2af53ee99c138906ba
Edwin Steele <edwin-lists@wordspeak.org>
2012-09-01 14:52:30 +1000

    Add canned data, and make the recent page use the same logic as the latest page

--- sensors/views.py
+++ sensors/views.py
@@ -5,7 +5,7 @@ from sensors.models import SensorReading
 import logging
 
 from datetime import date, datetime, timedelta
-from dateutil.tz import tzutc
+from dateutil.tz import tzutc, tzlocal
 
 logger = logging.getLogger(__name__)
 
@@ -28,18 +28,35 @@ def day_detail(request, year, month, day):
         ymd = None
     return HttpResponse("You're at the day detail for %s/%s/%s (%s)." % (day, month, year, ymd))
 
-def latest_detail(request):
-    """
-    stuffs
-    """
+def generic_detail(request, recency_timedelta):
     # TODO: Make more resilient if there aren't any database objects
     # TODO: is this really the most efficient way to get the most recent reading?
     latest_reading = SensorReading.objects.all().order_by('-datetime_read')[:1][0]
     # Are there any cases where latest reading might not be included in last_hour_readings?
-    one_hour_ago = datetime.now(tz=tzutc()) - timedelta(seconds=3600)
-    logger.debug("One hour ago: %s" % (one_hour_ago,))
-    last_hour_readings = SensorReading.objects.filter(datetime_read__gte=one_hour_ago)
+    logger.debug("Processing data starting from: %s" % (recency_timedelta,))
+    recency_readings = SensorReading.objects.filter(datetime_read__gte=recency_timedelta)
     return render_to_response('sensors/one_reading.html', {
         'one_reading': latest_reading,
-        'reading_context': last_hour_readings,
-    }, context_instance=RequestContext(request))
+        'reading_context': recency_readings,
+        }, context_instance=RequestContext(request))
+
+def recent_detail(request):
+    twenty_four_hours_ago = datetime.now(tz=tzutc()) - timedelta(days=1)
+    return generic_detail(request, twenty_four_hours_ago)
+
+def canned_detail(request):
+    # This is a full day of data and is useful for testing
+    most_recent_reading = SensorReading.objects.all().order_by('-datetime_read')[:1][0]
+    earliest_reading = datetime(2012, 8, 14, 0, 0, 1, tzinfo=tzlocal())
+    latest_reading = datetime(2012, 8, 14, 23, 59, 59, tzinfo=tzlocal())
+    recency_readings = SensorReading.objects.filter(datetime_read__gte=earliest_reading, datetime_read__lte=latest_reading)
+    return render_to_response('sensors/one_reading.html', {
+        'one_reading': most_recent_reading,
+        'reading_context': recency_readings,
+        }, context_instance=RequestContext(request))
+
+
+
+def latest_detail(request):
+    one_hour_ago = datetime.now(tz=tzutc()) - timedelta(seconds=3600)
+    return generic_detail(request, one_hour_ago)
