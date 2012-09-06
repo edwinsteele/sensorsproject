946fc50d26ee3e3e2843c2c03f6c70ed207534a3
Edwin Steele <edwin-lists@wordspeak.org>
2012-09-06 18:14:37 +1000

    Remove custom logging setup now that django-debug toolbar is showing what I want

--- sensorsproject/settings.py
+++ sensorsproject/settings.py
@@ -131,46 +131,24 @@ INSTALLED_APPS = (
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'standard': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        },
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
-        'null': {
-            'level':'DEBUG',
-            'class':'django.utils.log.NullHandler',
-            },
-        'logfile': {
-            'level':'DEBUG',
-            'class':'logging.handlers.RotatingFileHandler',
-            'filename': "logs/default.log",
-            'maxBytes': 50000,
-            'backupCount': 2,
-            'formatter': 'standard',
-            },
-        'console':{
-            'level':'INFO',
-            'class':'logging.StreamHandler',
-            'formatter': 'standard'
-        },
-        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
     'loggers': {
-        'django': {
-            'handlers':['console'],
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
             'propagate': True,
-            'level':'WARN',
-            },
-        'django.db.backends': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-            },
-        'sensors': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
             },
         }
 }
