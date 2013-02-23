e793f2d47eecc638aa10b1f2d37d693d85d36444
Edwin Steele <edwin-lists@wordspeak.org>
2013-02-23 13:31:16 +1100

    Populate prod settings with correct stuff

--- sensorsproject/settings/prod.py
+++ sensorsproject/settings/prod.py
@@ -1,11 +1,14 @@
 __author__ = 'esteele'
 
+# Common settings
+from .base import *
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'esteele_sensors_sensors',
-        'USER': '',
-        'PASSWORD': '',
+        'USER': get_env_variable("SENSORS_DB_USER"),
+        'PASSWORD': get_env_variable("SENSORS_DB_PASSWORD"),
         'HOST': 'localhost',
         'PORT': '',
     }
